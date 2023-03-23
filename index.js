eadd1164f3e46145d03763f6c29152099fbe565f
Andrea Stagi <stagi.andrea@gmail.com>
2023-03-23 21:33:12 +0100

    feat: add function to get unicorns

--- index.js
+++ index.js
@@ -6,3 +6,12 @@
 exports.getStars = function (numberOfStars) {
   return "🌟".repeat(numberOfStars);
 };
+
+/**
+ * getUnicorns is function to get a string containing the amount of unicorns you want.
+ * @function getUnicorns
+ * @param {number} numberOfUnicorns - The number of unicorns you want to display.
+ */
+exports.getUnicorns = function (numberOfUnicorns) {
+  return "🦄".repeat(numberOfUnicorns);
+};
--- test/test-lib.js
+++ test/test-lib.js
@@ -1,11 +1,21 @@
 const tap = require("tap");
-const { getStars } = require("../index.js");
+const { getStars, getUnicorns } = require("../index.js");
 
 tap.equal(getStars(1), "🌟");
 tap.equal(getStars(3), "🌟🌟🌟");
 tap.throws(
   function () {
     getStars(-1);
+  },
+  {},
+  { skip: true }
+);
+
+tap.equal(getUnicorns(1), "🦄");
+tap.equal(getUnicorns(3), "🦄🦄🦄");
+tap.throws(
+  function () {
+    getUnicorns(-1);
   },
   {},
   { skip: true }
